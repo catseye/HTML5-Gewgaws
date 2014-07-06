62d770259f979bd0af4f89bde88cdce7886eb2bd
Chris Pressey <cpressey@catseye.tc>
2014-07-06 21:42:44 +0100

    Clean up Progression slightly.

--- progression/progression.js
+++ progression/progression.js
@@ -4,32 +4,28 @@ function ProgressionController() {
     var ctx;
 
     this.draw = function(timeElapsed) {
-      var counter_elem = document.getElementById('counter');
       if (counter_elem) {
           counter_elem.innerHTML = Math.floor(counter);
-              // + "<br/>" + timeElapsed;
       }
-
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-
       ctx.beginPath();
-      ctx.lineWidth = "1";
+      ctx.lineWidth = 1;
       ctx.strokeStyle = "black";
-      ctx.moveTo(0,0);
+      ctx.moveTo(0, 0);
       var y = 200;
       var w = (canvas.width / counter);
       for (var i = 1; i <= counter; i++) {
           ctx.lineTo(w * i, y);
           y = (y == 200 ? 0 : 200);
       }
-      ctx.lineTo(canvas.width,0);
+      ctx.lineTo(canvas.width, 0);
       ctx.stroke();
-
       counter += timeElapsed / 60.0;
     };
 
-    this.start = function(c) {
+    this.start = function(c, counter_elem) {
       canvas = c;
+      this.counter_elem = counter_elem;
       ctx = canvas.getContext('2d');
       counter = 1;
       this.animation = (new yoob.Animation()).init({
@@ -39,4 +35,3 @@ function ProgressionController() {
       this.animation.start();
     };
 }
-
